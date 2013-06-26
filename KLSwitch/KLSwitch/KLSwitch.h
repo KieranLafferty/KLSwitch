56dac9837079acd633053895fc4b9aa93884a5c5
Kieran Lafferty <kieran.lafferty@gmail.com>
2013-06-25 21:33:34 -0400

    First pass at refactoring

--- KLSwitch/KLSwitch/KLSwitch.h
+++ KLSwitch/KLSwitch/KLSwitch.h
@@ -13,12 +13,14 @@ typedef void(^changeHandler)(BOOL isOn);
 
 @interface KLSwitch : UIControl <NSCoding>
 //UISwitch interface
-@property(nonatomic, strong) UIImage *onImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;         //Currently this do nothing
-@property(nonatomic, strong) UIImage *offImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;        //Currently this do nothing
+@property(nonatomic, strong) UIImage *onImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;         //Currently this does nothing
+@property(nonatomic, strong) UIImage *offImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;        //Currently this does nothing
 
 @property(nonatomic, strong) UIColor *onTintColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong) UIColor *tintColor NS_AVAILABLE_IOS(6_0) UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong) UIColor *thumbTintColor NS_AVAILABLE_IOS(6_0) UI_APPEARANCE_SELECTOR;
+
+
 ///Additional color options provided by KLSwitch only
 @property(nonatomic, strong) UIColor *contrastColor;
 @property(nonatomic, strong) UIColor *thumbBorderColor;
@@ -31,7 +33,10 @@ typedef void(^changeHandler)(BOOL isOn);
 
 //Percent (0.0 - 1.0) of the control to travel while panning before a switch toggle is activated
 @property(nonatomic, assign) CGFloat panActivationThreshold;
+
+//Set to true if you want to maintain 51x31 proportions, false if you want to set the frame to anything
 @property(nonatomic, assign) BOOL shouldConstrainFrame;
+
 //Initializers
 - (id)initWithFrame:(CGRect)frame;
 - (id)initWithFrame:(CGRect)frame
@@ -40,30 +45,5 @@ typedef void(^changeHandler)(BOOL isOn);
 //Events
 - (void)setOn:(BOOL)on animated:(BOOL)animated;
 
-@end
-
-@interface KLSwitchKnob : UIView
--(id) initWithParentSwitch:(KLSwitch*) parentSwitch;
--(void) setIsTracking:(BOOL)isTracking
-             animated:(BOOL) animated;
-@property (nonatomic, weak) KLSwitch* parentSwitch;
-@property (nonatomic, assign) BOOL isTracking;
--(CGRect) trackingFrameForSwitch:(KLSwitch*) parentSwitch;
--(CGRect) frameForCurrentStateForSwitch:(KLSwitch*) parentSwitch;
-@end
-
-@interface KLSwitchTrack : UIView
-@property(nonatomic, getter=isOn) BOOL on;
-@property (nonatomic, strong) UIColor* contrastColor;
-@property (nonatomic, strong) UIColor* onTintColor;
-@property (nonatomic, strong) UIColor* tintColor;
--(id) initWithFrame:(CGRect)frame
-            onColor:(UIColor*) onColor
-           offColor:(UIColor*) offColor
-      contrastColor:(UIColor*) contrastColor;
--(void) growContrastView;
--(void) shrinkContrastView;
--(void) setOn:(BOOL) on
-     animated:(BOOL) animated;
 @end
 
